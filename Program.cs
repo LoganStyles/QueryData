6348b262d28cee59e62cc1f6902d12f6460b315d
LoganStyles <emmaist23@gmail.com>
2022-07-19 07:03:25 +0100

    feat(query-operation): formatted program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,9 @@ int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
 
 // 2. Query creation.
 // numberQuery is an IEnumerable<int>
-var numberQuery = from num in numbers where (num % 2) != 0 select num;
+var numberQuery = from num in numbers 
+                    where (num % 2) != 0 
+                    select num;
 
 // 3. Query execution.
 foreach (int num in numberQuery)
