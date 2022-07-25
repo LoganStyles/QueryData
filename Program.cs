0fe018d3da7b4cd10e9fd5526699f5bf293e99e5
LoganStyles <emmaist23@gmail.com>
2022-07-25 20:19:20 +0100

    feat(linq projecting): projecting with query syntax contd

--- Program.cs
+++ Program.cs
@@ -1,28 +1,18 @@
 ﻿//data source
 List<int> grades = new List<int>() {20,50,60,81,95,32,45};
 
-//query for ascending sort
-var lowScoresQuery = from grade in grades
-where grade > 50
-select grade;
+//query for high scores
+var highScoresQuery = from grade in grades
+                        where grade > 69
+                        select grade;
 
-//query for descending sort
-var descendingFruitsQuery = fruits
-                            .OrderByDescending(fruit=>fruit); // descending order
 
 //execute the query
-Console.WriteLine("Ascending Order:");
-foreach (string item in ascendingFruitsQuery)
+foreach (var item in highScoresQuery)
 {
     Console.WriteLine(item);
 }
 
-//execute the query
-Console.WriteLine(Environment.NewLine + "Descending Order:");
-foreach (string item in descendingFruitsQuery)
-{
-    Console.WriteLine(item);
-}
 
 
 
