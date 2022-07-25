26a1eb9ba83a8418ca7a6ba4d87d5476159f3fa4
LoganStyles <emmaist23@gmail.com>
2022-07-25 20:54:01 +0100

    feat(linq grouping): grouping with query syntax

--- Program.cs
+++ Program.cs
@@ -1,16 +1,33 @@
 ﻿//data source
-List<int> grades = new List<int>() {20,50,60,81,95,32,45};
+List<Student> students = new List<Student> {
+    new Student {FirstName="Felix", LastName="Donald", Grade=70},
+    new Student {FirstName="Sandra", LastName="Yemi", Grade=54},
+    new Student {FirstName="Jones", LastName="Blaze", Grade=61},
+    new Student {FirstName="Charles", LastName="Fraser", Grade=43}
+
+    };
 
 //query for high scores
-var highScoresQuery = grades.Where(g=>g > 69).Select(g=>g);
+var studentGroupQuery = from student in students
+group student by student.Grade >=50
+into g
+select g;
 
 //execute the query
-foreach (var item in highScoresQuery)
+foreach (var group in studentGroupQuery)
 {
-    Console.WriteLine(item);
+    // Console.WriteLine(group.Key);
+    Console.WriteLine(group.Key == true ? "High scores" : "Low scores");
+
+    foreach(var scoreGroup in group)
+    Console.WriteLine("\t{0}", scoreGroup.FirstName);
 }
 
-
+public class Student{
+    public string FirstName {get; set;}
+    public string LastName {get; set;}
+    public int Grade {get; set;}
+}
 
 
 
