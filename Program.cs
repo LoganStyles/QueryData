d5cf9af53f90505210f9b17e6d530ea579b4c718
LoganStyles <emmaist23@gmail.com>
2022-07-25 20:31:28 +0100

    feat(linq projecting): projecting with query method

--- Program.cs
+++ Program.cs
@@ -2,10 +2,7 @@
 List<int> grades = new List<int>() {20,50,60,81,95,32,45};
 
 //query for high scores
-var highScoresQuery = from grade in grades
-                        where grade > 69
-                        select grade;
-
+var highScoresQuery = grades.Where(g=>g > 69).Select(g=>g);
 
 //execute the query
 foreach (var item in highScoresQuery)
