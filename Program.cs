8e42b05b4cedfb9723fa427485eeeb7a64c8cf66
LoganStyles <emmaist23@gmail.com>
2022-07-26 12:16:37 +0100

    feat(linq grouping): formatted program.cs

--- Program.cs
+++ Program.cs
@@ -1,24 +1,29 @@
-﻿//data source
-List<Employee> Employees = new List<Employee> {
-    new Employee {FirstName="Felix", LastName="Donald", Salary=7000},
-    new Employee {FirstName="Sandra", LastName="Yemi", Salary=5400},
-    new Employee {FirstName="Jones", LastName="Blaze", Salary=6100},
-    new Employee {FirstName="Charles", LastName="Fraser", Salary=4300}
-
-    };
-
-//query to group Employees
-var EmployeeSalaryQuery = Employees
-                        .GroupBy(e=>e.Salary >=5000);
-
-
-//execute the query
-foreach (var group in EmployeeSalaryQuery)
+﻿class Program
 {
-    Console.WriteLine(group.Key == true ? "High earning employee(s)" : "Low earning employee(s)");
+    private static void Main(string[] args)
+    {
+        //data source
+        List<Employee> employees =  new List<Employee> {
+                                    new Employee {FirstName="Felix", LastName="Donald", Salary=7000},
+                                    new Employee {FirstName="Sandra", LastName="Yemi", Salary=5400},
+                                    new Employee {FirstName="Femi", LastName="Fraser", Salary=4300},
+                                    new Employee {FirstName="Jones", LastName="Blaze", Salary=6100}
+                                    };
 
-    foreach(var salaryGroup in group)
-    Console.WriteLine("\t{0}", salaryGroup.FirstName);
+        //query to group Employees
+        var employeeSalaryQuery = employees
+                                .GroupBy(e => e.Salary >= 5000);
+
+
+        //execute the query
+        foreach (var group in employeeSalaryQuery)
+        {
+            Console.WriteLine(group.Key == true ? "High earning employee(s)" : "Low earning employee(s)");
+
+            foreach(var item in group)
+            Console.WriteLine("\t{0} \t{1}", item.FirstName, item.Salary);
+        }
+    }
 }
 
 public class Employee{
