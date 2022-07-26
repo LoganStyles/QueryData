43678413be0ab3e8003afb62a85fa3ddb7267ce1
LoganStyles <emmaist23@gmail.com>
2022-07-26 08:59:43 +0100

    feat(linq grouping): converted to using program class

--- Program.cs
+++ Program.cs
@@ -1,26 +1,32 @@
-﻿//data source
-List<Student> students = new List<Student> {
-    new Student {FirstName="Felix", LastName="Donald", Grade=70},
-    new Student {FirstName="Sandra", LastName="Yemi", Grade=54},
-    new Student {FirstName="Jones", LastName="Blaze", Grade=61},
-    new Student {FirstName="Charles", LastName="Fraser", Grade=43}
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //data source
+        List<Student> students = new List<Student> {
+                                        new Student {FirstName="Felix", LastName="Donald", Grade=70},
+                                        new Student {FirstName="Sandra", LastName="Yemi", Grade=54},
+                                        new Student {FirstName="Jones", LastName="Blaze", Grade=61},
+                                        new Student {FirstName="Charles", LastName="Fraser", Grade=43}
 
     };
 
-//query for high scores
-var studentGroupQuery = from student in students
-group student by student.Grade >=50
-into g
-select g;
+        //query for high scores
+        var studentGroupQuery = from student in students
+                                group student by student.Grade >= 50;
+                                // into g
+                                // select g;
 
-//execute the query
-foreach (var group in studentGroupQuery)
-{
-    // Console.WriteLine(group.Key);
-    Console.WriteLine(group.Key == true ? "High scores" : "Low scores");
+        //execute the query
+        foreach (var group in studentGroupQuery)
+        {
+            // Console.WriteLine(group.Key);
+            Console.WriteLine(group.Key == true ? "High scores" : "Low scores");
 
-    foreach(var scoreGroup in group)
-    Console.WriteLine("\t{0}", scoreGroup.FirstName);
+            foreach (var scoreGroup in group)
+                Console.WriteLine("\t{0}", scoreGroup.FirstName);
+        }
+    }
 }
 
 public class Student{
