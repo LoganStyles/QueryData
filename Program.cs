94e2210bfed746f032d638036a86752d49ec9541
LoganStyles <emmaist23@gmail.com>
2022-07-26 11:29:55 +0100

    feat(linq grouping): changed students to employees

--- Program.cs
+++ Program.cs
@@ -1,30 +1,30 @@
 ﻿//data source
-List<Student> students = new List<Student> {
-    new Student {FirstName="Felix", LastName="Donald", Grade=70},
-    new Student {FirstName="Sandra", LastName="Yemi", Grade=54},
-    new Student {FirstName="Jones", LastName="Blaze", Grade=61},
-    new Student {FirstName="Charles", LastName="Fraser", Grade=43}
+List<Employee> Employees = new List<Employee> {
+    new Employee {FirstName="Felix", LastName="Donald", Salary=7000},
+    new Employee {FirstName="Sandra", LastName="Yemi", Salary=5400},
+    new Employee {FirstName="Jones", LastName="Blaze", Salary=6100},
+    new Employee {FirstName="Charles", LastName="Fraser", Salary=4300}
 
     };
 
-//query to group students
-var studentGroupQuery = students
-                        .GroupBy(student=>student.Grade >=50);
+//query to group Employees
+var EmployeeSalaryQuery = Employees
+                        .GroupBy(e=>e.Salary >=5000);
 
 
 //execute the query
-foreach (var group in studentGroupQuery)
+foreach (var group in EmployeeSalaryQuery)
 {
-    Console.WriteLine(group.Key == true ? "Students who passed" : "Students who failed");
+    Console.WriteLine(group.Key == true ? "High earning employee(s)" : "Low earning employee(s)");
 
-    foreach(var scoreGroup in group)
-    Console.WriteLine("\t{0}", scoreGroup.FirstName);
+    foreach(var salaryGroup in group)
+    Console.WriteLine("\t{0}", salaryGroup.FirstName);
 }
 
-public class Student{
+public class Employee{
     public string FirstName {get; set;}
     public string LastName {get; set;}
-    public int Grade {get; set;}
+    public int Salary {get; set;}
 }
 
 
