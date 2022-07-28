608b7f1eceb6e93db91e09f6ba594353b2f0303b
LoganStyles <emmaist23@gmail.com>
2022-07-28 15:23:32 +0100

    feat(joins): inner joins with query method - modified album.PublishId to album.PublisherId

--- Program.cs
+++ Program.cs
@@ -14,15 +14,15 @@
         //data source
         List<Album> albums = new List<Album>
                                 {
-                                    new Album {Id=1, PublishId=2, Title="Gold Rush"},
-                                    new Album {Id=2, PublishId=3, Title="My love"},
-                                    new Album {Id=3, PublishId=3, Title="1000 miles"},
-                                    new Album {Id=4, PublishId=2, Title="Sunny roses"}
+                                    new Album {Id=1, PublisherId=2, Title="Gold Rush"},
+                                    new Album {Id=2, PublisherId=3, Title="My love"},
+                                    new Album {Id=3, PublisherId=3, Title="1000 miles"},
+                                    new Album {Id=4, PublisherId=2, Title="Sunny roses"}
                                 };
 
-        //inner join query between albums and Publishers
+        //join albums and publishers based on PublisherId
         var albumPublisherQuery = albums
-                                        .Join(publishers,a=>a.PublishId,p=>p.Id, (alb,pub)=>new
+                                        .Join(publishers,a=>a.PublisherId,p=>p.Id, (alb,pub)=>new
                                                 {
                                                 AlbumTitle  =alb.Title,
                                                 PublisherName = pub.FirstName+" "+pub.LastName
