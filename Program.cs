66440557c1228eba23b5c48a82b65542f77e6dd9
LoganStyles <emmaist23@gmail.com>
2022-08-10 15:47:49 +0100

    feat(related-data): testing eager loading - added multiple includes

--- Program.cs
+++ Program.cs
@@ -7,15 +7,29 @@ class Program
     {
         var context = new ArtistsContext();
 
-        //eager loading
+        //eager loading - basic example
         var selectedEmployees = context.Employees
                                 .Include(emp => emp.Studio)
+                                .Include(emp => emp.Albums)
                                 .ToList();
 
+        // System.Console.WriteLine();
+        // foreach (var emp in selectedEmployees)
+        // {
+        //     System.Console.WriteLine("{0} works at {1} office.", emp.LastName, emp.Studio.City);
+        // }
+        
+        //eager loading - multiple includes
         System.Console.WriteLine();
         foreach (var emp in selectedEmployees)
         {
-            System.Console.WriteLine("{0} works at {1}", emp.FirstName+" "+emp.LastName, emp.Studio.City);
+            Console.WriteLine("{0} works at {1} office and has the following album(s).", emp.LastName, emp.Studio.City);
+
+            foreach(var album in emp.Albums)
+            {
+                System.Console.WriteLine(album.Title);
+            }
+            System.Console.WriteLine();
         }
     }
 }
