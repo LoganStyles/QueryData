1f6dc0651dbcc24e07cfaab36d68cbbe35d759a2
LoganStyles <emmaist23@gmail.com>
2022-08-10 09:57:33 +0100

    feat(query-operation-quiz): question on partitioning

--- Program.cs
+++ Program.cs
@@ -7,14 +7,21 @@ class Program
     {
         int[] numbers1 = { 17, 12, 5, 21, 30 };
 
-        var query = from element in numbers1 orderby element  select element;
+        // var query = from element in numbers1 orderby element  select element;
 
-        int[] numbers2 = query.ToArray();
+        // int[] numbers2 = query.ToArray();
 
-        foreach (int value in numbers2)
+        // foreach (int value in numbers2)
+        // {
+        //     Console.Write("{0} ",value);
+        // }
+        //     Console.WriteLine();
+
+        var selection = numbers1.OrderBy(n => n).Skip(2).Take(2);
+        foreach (int item in selection)
         {
-            Console.Write("{0} ",value);
+            Console.Write("{0} ", item);
         }
-            Console.WriteLine();
+        Console.WriteLine();
     }
 }
