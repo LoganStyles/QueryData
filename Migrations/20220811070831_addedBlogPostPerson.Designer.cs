b208181e5739a404e1812839342fed58b12c7096
LoganStyles <emmaist23@gmail.com>
2022-08-11 09:01:18 +0100

    feat(related-data): testing eager loading - added migrations for blogs, posts, & person entities & pushed to the db

--- Migrations/20220811070831_addedBlogPostPerson.Designer.cs
+++ Migrations/20220811070831_addedBlogPostPerson.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QueryData;
 
@@ -10,9 +11,10 @@ using QueryData;
 namespace QueryData.Migrations
 {
     [DbContext(typeof(ArtistsContext))]
-    partial class ArtistsContextModelSnapshot : ModelSnapshot
+    [Migration("20220811070831_addedBlogPostPerson")]
+    partial class addedBlogPostPerson
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.2");
