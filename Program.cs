d3a3b8ca16bff93ba7f0879aa24d6faf49183574
LoganStyles <emmaist23@gmail.com>
2022-08-11 14:20:08 +0100

    feat(related data): eager loading - created 3 code examples

--- Program.cs
+++ Program.cs
@@ -7,14 +7,47 @@ class Program
     {
         var context = new ArtistsContext();
 
-        //eager loading - multiple includes
+        #region  eager loading
+        // var selectedEmployees = context.Employees
+        //     .Include(emp => emp.Studio) //including a reference navigation property
+        //     .ToList();
+
+        // foreach (var emp in selectedEmployees)
+        // {
+        //     Console.WriteLine("{0} works at the {1} office.", emp.LastName, emp.Studio.City);
+        //     System.Console.WriteLine();
+        // }
+        #endregion
+
+        #region  eager loading - multiple includes
+        // var selectedEmployees = context.Employees
+        //     .Include(emp => emp.Studio) //including a reference navigation property
+        //     .Include(emp => emp.Albums) //including a collection property
+        //     .ToList();
+
+        // foreach (var emp in selectedEmployees)
+        // {
+        //     Console.WriteLine(
+        //         "{0} works at the {1} office and has the following album(s).",
+        //         emp.LastName,
+        //         emp.Studio.City
+        //     );
+
+        //     foreach (var album in emp.Albums)
+        //     {
+        //         System.Console.WriteLine(album.Title);
+        //     }
+        //         System.Console.WriteLine();
+        // }
+        #endregion
+
+        #region  eager loading - multiple includes & drilling down multiple levels
         var selectedEmployees = context.Employees
             .Include(emp => emp.Studio) //including a reference navigation property
             .Include(emp => emp.Albums) //including a collection property
             .ThenInclude(alb => alb.Tags) //drilling down multiple levels
             .ToList();
 
-        System.Console.WriteLine();
         foreach (var emp in selectedEmployees)
         {
             Console.WriteLine(
@@ -30,10 +63,13 @@ class Program
 
                 foreach (var tags in album.Tags)
                 {
-                    System.Console.Write("{0} ",tags.Title);
+                    System.Console.Write("{0} ", tags.Title);
                 }
+                System.Console.WriteLine();
+                System.Console.WriteLine();
             }
-            System.Console.WriteLine();
+        System.Console.WriteLine();
         }
+        #endregion
     }
 }
