1eab41d15470feef342c049c5d2cd60115dd3851
LoganStyles <emmaist23@gmail.com>
2022-08-11 09:19:32 +0100

    feat(related-data): testing eager loading - edited program.cs to reflect include & ThenInclude

--- Program.cs
+++ Program.cs
@@ -7,27 +7,31 @@ class Program
     {
         var context = new ArtistsContext();
 
-        //eager loading - basic example
-        var selectedEmployees = context.Employees
-                                .Include(emp => emp.Studio)
-                                .Include(emp => emp.Albums)
-                                .ToList();
-
-        // System.Console.WriteLine();
-        // foreach (var emp in selectedEmployees)
-        // {
-        //     System.Console.WriteLine("{0} works at {1} office.", emp.LastName, emp.Studio.City);
-        // }
-        
         //eager loading - multiple includes
+        var selectedEmployees = context.Employees
+            .Include(emp => emp.Studio) //including a reference navigation property
+            .Include(emp => emp.Albums) //including a collection property
+            .ThenInclude(alb => alb.Tags) //drilling down multiple levels
+            .ToList();
+
         System.Console.WriteLine();
         foreach (var emp in selectedEmployees)
         {
-            Console.WriteLine("{0} works at {1} office and has the following album(s).", emp.LastName, emp.Studio.City);
+            Console.WriteLine(
+                "{0} works at the {1} office and has the following album(s).",
+                emp.LastName,
+                emp.Studio.City
+            );
 
-            foreach(var album in emp.Albums)
+            foreach (var album in emp.Albums)
             {
                 System.Console.WriteLine(album.Title);
+                System.Console.WriteLine("\tThis album has the following tags");
+
+                foreach (var tags in album.Tags)
+                {
+                    System.Console.WriteLine("\t\t{0}",tags.Title);
+                }
             }
             System.Console.WriteLine();
         }
