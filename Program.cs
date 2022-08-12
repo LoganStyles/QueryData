60f232a7c499ccfc5669dd68ce6d12b5085e51da
LoganStyles <emmaist23@gmail.com>
2022-08-12 09:25:26 +0100

    feat(related data): eager loading - formatted code for adding multiple levels of related entities

--- Program.cs
+++ Program.cs
@@ -7,35 +7,26 @@ class Program
     {
         var context = new ArtistsContext();
 
-        
         //drilling down multiple levels
         var selectedEmployees = context.Employees
-            .Include(emp => emp.Studio) //including a reference navigation property
             .Include(emp => emp.Albums) //including a collection property
             .ThenInclude(alb => alb.Tags) //drilling down multiple levels
             .ToList();
 
         foreach (var emp in selectedEmployees)
         {
-            Console.WriteLine(
-                "{0} works at the {1} office and has the following album(s).",
-                emp.LastName,
-                emp.Studio.City
-            );
+            Console.WriteLine("{0} has the following album(s).", emp.LastName);
 
             foreach (var album in emp.Albums)
             {
-                System.Console.WriteLine(album.Title);
-                System.Console.WriteLine("This album has the following tag(s)");
+                Console.WriteLine("{0} - This album has the following tag(s)", album.Title);
 
                 foreach (var tags in album.Tags)
                 {
-                    System.Console.Write("{0} ", tags.Title);
+                    Console.WriteLine("{0} ", tags.Title);
                 }
-                System.Console.WriteLine();
-                System.Console.WriteLine();
             }
-        System.Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
