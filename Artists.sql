564be60021fb4632b7fabf490273b3888bc5bfda
LoganStyles <emmaist23@gmail.com>
2022-08-15 09:26:00 +0100

    feat(related data): eager loading - corrected my previous error of leaving out the create statements in Artists.sql

--- Artists.sql
+++ Artists.sql
@@ -1,4 +1,38 @@
-
+CREATE TABLE IF NOT EXISTS Employees (
+    "Id" INTEGER NOT NULL,
+    "FirstName" TEXT NOT NULL,
+    "LastName" TEXT NOT NULL,
+    "Age" INTEGER NOT NULL,
+    PRIMARY KEY("Id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS Studios (
+    "Id" INTEGER NOT NULL,
+    "HouseNumber" INTEGER NOT NULL,
+    "City" TEXT NOT NULL,
+    "EmployeeId" INTEGER NOT NULL UNIQUE,
+    PRIMARY KEY("Id" AUTOINCREMENT),
+    FOREIGN KEY (EmployeeId) REFERENCES Employees (Id)
+);
+CREATE TABLE IF NOT EXISTS Albums (
+    "Id" INTEGER NOT NULL,
+    "Title" TEXT NOT NULL,
+    "Price" REAL NOT NULL,
+    "EmployeeId" INTEGER NOT NULL,
+    PRIMARY KEY("Id" AUTOINCREMENT),
+    FOREIGN KEY (EmployeeId) REFERENCES Employees (Id)
+);
+CREATE TABLE IF NOT EXISTS Tags (
+    "Id" INTEGER NOT NULL,
+    "Title" TEXT NOT NULL,
+    PRIMARY KEY("Id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS AlbumTags(
+    AlbumId INTEGER,
+    TagId INTEGER,
+    FOREIGN KEY(AlbumId) REFERENCES Albums(Id) ON DELETE CASCADE,
+    FOREIGN KEY(TagId) REFERENCES Tags(Id) ON DELETE CASCADE,
+    PRIMARY KEY(AlbumId, TagId)
+);
 INSERT INTO Employees values(1,'Max','Bello',25);
 INSERT INTO Employees values(2,'Francis','Ojukwu',34);
 INSERT INTO Employees values(3,'Martha','Bertha',19);
