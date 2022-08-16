88ddabdf618917ea0b60118b21bda794f2ac4b0d
LoganStyles <emmaist23@gmail.com>
2022-08-16 10:09:14 +0100

    feat(related-data): testing eager loading - formatted ArtistsContext

--- Data/ArtistsContext.cs
+++ Data/ArtistsContext.cs
@@ -4,14 +4,9 @@ namespace QueryData
 {
     public partial class ArtistsContext : DbContext
     {
-        public ArtistsContext()
-        {
-        }
+        public ArtistsContext() { }
 
-        public ArtistsContext(DbContextOptions<ArtistsContext> options)
-            : base(options)
-        {
-        }
+        public ArtistsContext(DbContextOptions<ArtistsContext> options) : base(options) { }
 
         public virtual DbSet<Album> Albums { get; set; }
         public virtual DbSet<Employee> Employees { get; set; }
@@ -19,16 +14,19 @@ namespace QueryData
         public virtual DbSet<Tag> Tags { get; set; }
         public virtual DbSet<Blog> Blogs { get; set; }
         public virtual DbSet<Post> Posts { get; set; }
-        
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlite("data source=output/Artists.db")
-                .LogTo(Console.WriteLine, new[]{DbLoggerCategory.Database.Command.Name}, Microsoft.Extensions.Logging.LogLevel.Information)
-                .EnableSensitiveDataLogging();
+                optionsBuilder
+                    .UseSqlite("data source=output/Artists.db")
+                    .LogTo(
+                        Console.WriteLine,
+                        new[] { DbLoggerCategory.Database.Command.Name },
+                        Microsoft.Extensions.Logging.LogLevel.Information
+                    );
             }
-            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -74,6 +72,5 @@ namespace QueryData
         }
 
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
-
     }
 }
