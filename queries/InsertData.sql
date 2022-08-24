f04256703a218ee1785a0d4b1257ceff5a5e8588
LoganStyles <emmaist23@gmail.com>
2022-08-24 16:10:08 +0100

    feat(split queries): removed queries dir from git

--- queries/InsertData.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO Employees values(1,'Max','Bello',25);
-INSERT INTO Employees values(2,'Francis','Ojukwu',34);
-INSERT INTO Employees values(3,'Martha','Bertha',19);
-
-INSERT INTO Albums values(1,'Blue Fire',2000,2);
-INSERT INTO Albums values(2,'Raging Heart',3850,3);
-INSERT INTO Albums values(3,'Fixated on you',4000,1);
