dd93fa8723078776af2ccff444fa5b22f723a5ea
LoganStyles <emmaist23@gmail.com>
2022-12-05 16:53:38 +0100

    modified Program.cs

--- Program.cs
+++ Program.cs
@@ -4,13 +4,13 @@ int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
 
 // 2. Query creation.
 // numberQuery is an IEnumerable<int>
-var numberQuery = from num in numbers 
-                    where (num % 2) != 0 
-                    select num;
+var numberQuery = from num in numbers
+                  where (num % 2) != 0
+                  select num;
 
 // 3. Query execution.
 foreach (int num in numberQuery)
 {
     Console.Write("{0} ", num);
 }
-    Console.WriteLine();
+Console.WriteLine();
