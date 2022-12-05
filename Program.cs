85e33ffe276a38faffebe296ef1b385f0def1cce
LoganStyles <emmaist23@gmail.com>
2022-12-05 17:00:11 +0100

    feat(removed spaces)

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
-﻿
-char[] letters = new char[6] { 'B', 'D', 'A', 'C', 'B', 'A' };
+﻿char[] letters = new char[6] { 'B', 'D', 'A', 'C', 'B', 'A' };
 
 // letterQuery is an IEnumerable<char>
-var letterQuery = from letter in letters 
-                    where letter == 'A' 
-                    select letter;
+var letterQuery = from letter in letters
+                  where letter == 'A'
+                  select letter;
 
 foreach (char item in letterQuery)
 {
     Console.Write("{0} ", item);
 }
-    Console.WriteLine();
+Console.WriteLine();
